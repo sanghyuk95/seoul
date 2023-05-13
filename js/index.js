24ec8d81461b05d1d1614e1eeebf7ed117357c53
Burny95 <shskse5@gmail.com>
2023-05-14 01:06:06 +0900

    헤더,서치 완성

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const $ad = document.querySelector(".ad");
 const $colseBtn = document.querySelector(".close-btn");
+const $wrap = document.querySelector(".wrap");
 
 $colseBtn.addEventListener("click", (e) => {
   $ad.classList.add("active");
+  $wrap.classList.add("active");
   setTimeout(() => {
     $ad.style.display = "none";
+    $wrap.classList.remove("active");
   }, 500);
 });
