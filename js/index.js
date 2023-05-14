5e563aad24001014a7bbbb10f83eede6307d0195
Burny95 <shskse5@naver.com>
2023-05-15 02:11:37 +0900

    배너 스와이퍼 완성

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ $colseBtn.addEventListener("click", (e) => {
     $wrap.classList.remove("active");
   }, 500);
 });
+
+const swiper = new Swiper(".swiper", {
+  direcyion: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    prevEl: ".swiper-control-pre",
+    nextEl: ".swiper-control-next",
+  },
+  pagination: {
+    el: ".swiper-control-page",
+    type: "fraction",
+  },
+});
+
+const $slidePlay = document.querySelector(".swiper-control-play");
+
+$slidePlay.addEventListener("click", () => {
+  $slidePlay.classList.toggle("active");
+  if ($slidePlay.classList.contains("active")) {
+    swiper.autoplay.pause();
+  } else {
+    swiper.autoplay.resume();
+  }
+});
+
+const $bannerBtnUp = document.querySelector(".banner-left-btn-up");
+const $bannerBtnDown = document.querySelector(".banner-left-btn-down");
+
+swiper.on("slideChange", () => {
+  $bannerBtnUp.classList.toggle("active", swiper.realIndex < 3);
+  $bannerBtnDown.classList.toggle("active", swiper.realIndex > 2);
+});
